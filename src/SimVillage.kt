0c935085bbde9da7235f1b6d8719983fbe781589
Joonghyeon Kim <jheloper9102@gmail.com>
2020-02-20 18:39:45 +0900

    익명함수를 이용해 SimVillage 환영 메시지를 출력하는 기능 추가

---
+++ src/SimVillage.kt
@@ -0,0 +1,10 @@
+/**
+ * @author joonghyeon.kim
+ */
+
+fun main(args: Array<String>) {
+    println({
+        val currentYear = 2020
+        "Welcome to SimVillage, chief! (copyright $currentYear)"
+    }())
+}
