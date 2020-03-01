253429c5f572087af73e5edbf23452bd9f0b4b2f
Joonghyeon Kim <jheloper9102@gmail.com>
2020-03-01 20:08:55 +0900

    enum 선언 및 enum과 when 표현식을 사용한 예제 코드 추가

--- src/KotlinInAction.kt
+++ src/KotlinInAction.kt
@@ -1,6 +1,7 @@
 /**
  * @author joonghyeon.kim
  */
+import Color.*
 
 fun main(args: Array<String>) {
     println("Hello, world!")
@@ -49,6 +50,12 @@ fun main(args: Array<String>) {
     val rectangle2 = Rectangle(100, 130)
     println("rectangle's height is ${rectangle2.height}, width is ${rectangle2.width}")
     println(rectangle2.isSquare)
+
+    println(Color.BLUE.rgb())
+    println(genMnemonic(Color.BLUE))
+    println(getWarmth(Color.ORANGE))
+    println(mix(BLUE, YELLOW))
+    println(mixOptimized(BLUE, YELLOW))
 }
 
 // 기본적인 함수를 선언하는 방법
@@ -77,4 +84,44 @@ class Rectangle(val height: Int, val width: Int) {
         get() {
             return height == width
         }
+}
+
+// enum 선언
+enum class Color(val r: Int, val g: Int, val b: Int) {
+    RED(255, 0, 0), ORANGE(255, 165, 0), YELLOW(255, 255, 0),
+    GREEN(0, 255, 0), BLUE(0, 0, 255), INDIGO(75, 0, 130), VIOLET(238, 130, 238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
+}
+
+// when 문법
+fun genMnemonic(color: Color) = when (color) {
+    Color.RED -> "Richard"
+    Color.ORANGE -> "Of"
+    Color.YELLOW -> "York"
+    Color.GREEN -> "Gave"
+    Color.BLUE -> "Battle"
+    Color.INDIGO -> "In"
+    Color.VIOLET -> "Vain"
+}
+
+fun getWarmth(color: Color) = when (color) {
+    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+    Color.GREEN -> "neutral"
+    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+}
+
+fun mix(c1: Color, c2: Color) = when (setOf(c1, c2)) {
+    setOf(RED, YELLOW) -> ORANGE
+    setOf(YELLOW, BLUE) -> GREEN
+    setOf(BLUE, VIOLET) -> INDIGO
+    else -> throw Exception("Dirty color")
+}
+
+// 인자 없는 when
+fun mixOptimized(c1: Color, c2: Color) = when {
+    (c1 == RED && c2 == YELLOW) || (c1 == YELLOW && c2 == RED) -> ORANGE
+    (c1 == YELLOW && c2 == BLUE) || (c1 == BLUE && c2 == YELLOW) -> GREEN
+    (c1 == BLUE && c2 == VIOLET) || (c1 == VIOLET && c2 == BLUE) -> INDIGO
+    else -> throw Exception("Dirty color")
 }
