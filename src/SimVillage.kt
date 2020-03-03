04fcb3a9772aa0372d9117b34322bfd08d5fd921
Joonghyeon Kim <jheloper9102@gmail.com>
2020-03-03 15:57:17 +0900

    하나의 인자 또는 여러 인자를 받는 익명 함수 및 it 키워드를 사용한 인자 참조에 대한 예제 코드 추가

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -15,7 +15,31 @@ fun main(args: Array<String>) {
         val currentYear = 2020
         "This village is SimVillage! (copyright $currentYear)"
     }
-
     println("greetingFunction variable type is '$greetingFunction'")
     println(greetingFunction())
+
+    // 인자 있는 익명 함수.
+    val greetingFunction2: (String) -> String = {
+        playerName ->
+        val currentYear = 2020
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingFunction2("Tommy"))
+
+    // it 키워드로 인자를 받는 익명 함수.
+    // it 키워드는 하나의 인자를 받을 때에만 사용할 수 있다.
+    val greetingFunction3: (String) -> String = {
+        val currentYear = 2020
+        "Welcome to SimVillage, $it! (copyright $currentYear)"
+    }
+    println(greetingFunction3("Helen"))
+
+    // 여러 인자를 받는 익명 함수.
+    val greetingFunction4: (String, Int) -> String = {
+        playerName, numBuildings ->
+        val currentYear = 2020
+        println("$numBuildings buildings added")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingFunction4("John", 3))
 }
