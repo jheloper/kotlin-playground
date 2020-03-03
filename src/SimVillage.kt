1ef06791228c274882249a31a8f2cc8c9e859222
Joonghyeon Kim <jheloper9102@gmail.com>
2020-03-03 15:45:08 +0900

    익명 함수를 함수 타입 변수에 지정하여 사용하는 예제 코드 추가

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -3,8 +3,19 @@
  */
 
 fun main(args: Array<String>) {
+    // 익명 함수를 선언하고 호출.
     println({
         val currentYear = 2020
         "Welcome to SimVillage, chief! (copyright $currentYear)"
     }())
+
+    // 익명 함수를 변수에 지정.
+    val greetingFunction: () -> String = {
+        // 익명 함수는 암시적 반환이기 때문에 return 키워드를 생략한다.
+        val currentYear = 2020
+        "This village is SimVillage! (copyright $currentYear)"
+    }
+
+    println("greetingFunction variable type is '$greetingFunction'")
+    println(greetingFunction())
 }
